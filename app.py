af216a8ec4806268a848b52216d2154bdf399582
jdg0812 <73633120+jdg0812@users.noreply.github.com>
2024-03-25 14:37:53 +1100

    initla commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify, render_template, url_for
+import joblib
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import yaml
+from settings import add_RUL_column
+from sklearn.model_selection import train_test_split
+import sqlite3
+
+
+app = Flask(__name__)
+
+with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+
+model = joblib.load('base_rf_pipe.joblib')
+
+
+
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+
+
+@app.route('/train', methods=['POST'])
+def train():
+
+    conn = sqlite3.connect('cmapss_dataset.db')
+    c = conn.cursor()
+    query = f"""SELECT * FROM cmapss_dataset"""
+    c.execute(query)
+    data = c.fetchall()
+    conn.close()
+    col_names = config['col_names']
+    train = pd.DataFrame(data, columns=col_names)
+    
+    rul = pd.read_csv('CMAPSSDATA/RUL_FD001.txt',sep='\s+',header=None,index_col=False,names=['RUL'])
+    train = add_RUL_column(train)
+
+    drop_labels = config['index_names']+config['setting_names']
+    X_train=train.drop(columns=drop_labels).copy()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_train.drop('RUL', axis=1), #predictor
+        X_train['RUL'], #target
+        test_size=0.3, #split
+        random_state=0) #set seed for reproducibility
+    
+    model.fit(X_train, y_train)
+    pred_train = model.predict(X_train)
+    train_rmse = np.sqrt(mean_squared_error(y_train, pred_train))
+    train_r2 = r2_score(y_train, pred_train)
+    train_mae = mean_absolute_error(y_train, pred_train)
+    pred_test = model.predict(X_test)
+    test_rmse = np.sqrt(mean_squared_error(y_test, pred_test))
+    test_r2 = r2_score(y_test, pred_test)
+    test_mae = mean_absolute_error(y_test, pred_test)
+    return render_template('index.html', train_rmse=train_rmse, train_r2=train_r2, train_mae=train_mae, test_rmse=test_rmse, test_r2=test_r2, test_mae=test_mae)
+
+
+
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
---
+++ data_ingestion.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+conn = sqlite3.connect('cmapss_dataset.db')
+cursor = conn.cursor()
+
+import pandas as pd
+import yaml
+
+with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+col_names = config['col_names']
+df = pd.read_csv('CMAPSSData/train_FD001.txt',sep='\s+',header=None,index_col=False,names=col_names)
+
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS cmapss_dataset (
+    unit_number INTEGER,
+    time_cycles INTEGER,
+    setting_1 REAL,
+    setting_2 REAL,
+    setting_3 REAL,
+    sensor_1 REAL,
+    sensor_2 REAL,
+    sensor_3 REAL,
+    sensor_4 REAL,
+    sensor_5 REAL,
+    sensor_6 REAL,
+    sensor_7 REAL,
+    sensor_8 REAL,
+    sensor_9 REAL,
+    sensor_10 REAL,
+    sensor_11 REAL,
+    sensor_12 REAL,
+    sensor_13 REAL,
+    sensor_14 REAL,
+    sensor_15 REAL,
+    sensor_16 REAL,
+    sensor_17 REAL,
+    sensor_18 REAL,
+    sensor_19 REAL,
+    sensor_20 REAL,
+    sensor_21 REAL
+)
+'''
+
+cursor.execute(create_table_query)
+
+for index, row in df.iterrows():
+    insert_query = '''
+    INSERT INTO cmapss_dataset VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+    '''
+    data_tuple = tuple(row)
+    cursor.execute(insert_query, data_tuple)
+
+
+# select_query = "SELECT * FROM cmapss_dataset"
+# cursor.execute(select_query)
+# results = cursor.fetchall()
+
+# for row in results: 
+#     print(row)
+
+conn.commit()
+cursor.close()
+conn.close()
